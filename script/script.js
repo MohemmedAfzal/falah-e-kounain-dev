97214f062572564e21f9b1112f65ebf8cab5c9da
mohaafza <afzalva37@gamil.com>
2024-07-29 22:26:46 +0530

    Working on style

--- script/script.js
+++ script/script.js
@@ -12,18 +12,17 @@ function fetchFarazNamazData(currentDate) {
     fetch('excel/namaz-timings.xlsx') // Adjust the path to your Excel file
         .then(response => response.arrayBuffer())
         .then(data => {
-            const workbook = XLSX.read(data, { type: 'array' });
+            const workbook = XLSX.read(data, {type: 'array'});
             const firstSheetName = workbook.SheetNames[0];
             const worksheet = workbook.Sheets[firstSheetName];
             const jsonData = XLSX.utils.sheet_to_json(worksheet);
 
-        
 
             const todaysTimings = jsonData.find(entry => {
-                const excelDate = new Date((entry.Date- (25567 + 2)) * 86400 * 1000); 
+                const excelDate = new Date((entry.Date - (25567 + 2)) * 86400 * 1000);
                 return excelDate.toDateString() === currentDate.toDateString();
             });
-            
+
             if (todaysTimings) {
                 document.getElementById('fajr-starts').textContent = todaysTimings['fajr-starts'];
                 document.getElementById('fajr-azan').textContent = todaysTimings['fajr-azan'];
@@ -56,18 +55,25 @@ function fetchNafilNamazData(currentDate) {
     fetch('excel/nafil-timings.xlsx') // Adjust the path to your Excel file
         .then(response => response.arrayBuffer())
         .then(data => {
-            const workbook = XLSX.read(data, { type: 'array' });
+            const workbook = XLSX.read(data, {type: 'array'});
             const firstSheetName = workbook.SheetNames[0];
             const worksheet = workbook.Sheets[firstSheetName];
             const jsonData = XLSX.utils.sheet_to_json(worksheet);
 
 
-
             const todaysTimings = jsonData.find(entry => {
-                const excelDate = new Date((entry.Date- (25567 + 2)) * 86400 * 1000);
+                const excelDate = new Date((entry.Date - (25567 + 2)) * 86400 * 1000);
                 return excelDate.toDateString() === currentDate.toDateString();
             });
-            console.log(todaysTimings)
+            const today = new Date();
+            const options = {
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric',
+                calendar: 'islamic',
+                timeZone: 'Asia/Kolkata'
+            };
+
             if (todaysTimings) {
                 document.getElementById('tahajjud-starts').textContent = todaysTimings['tahajjud-starts'];
                 document.getElementById('tahajjud-ends').textContent = todaysTimings['tahajjud-ends'];
@@ -77,6 +83,9 @@ function fetchNafilNamazData(currentDate) {
                 document.getElementById('chast-ends').textContent = todaysTimings['chast-ends'];
                 document.getElementById('awwabeen-starts').textContent = todaysTimings['awwabeen-starts'];
                 document.getElementById('awwabeen-ends').textContent = todaysTimings['awwabeen-ends'];
+                document.getElementById("date").textContent= new Intl.DateTimeFormat('en-TN-u-ca-islamic', options).format(today);
+                document.getElementById('sehri').textContent = todaysTimings['sehri'];
+                document.getElementById('iftari').textContent = todaysTimings['iftari'];
             } else {
                 document.getElementById('namaz-timings').innerHTML = '<p>No timings available for today.</p>';
             }
